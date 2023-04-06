ba2a5f2b96c2b7ddbc916db50e6939870f238964
Duchstf <minhduc8199@gmail.com>
2023-04-06 12:42:12 -0500

    fix the weights formatting

--- L1Trigger/Phase2L1ParticleFlow/interface/taus/weights/b4.h
+++ L1Trigger/Phase2L1ParticleFlow/interface/taus/weights/b4.h
@@ -3,13 +3,9 @@
 //Max 0.023343238980
 //Number of zeros 0
 
-#ifndef B8_H_
-#define B8_H_
+#ifndef B4_H_
+#define B4_H_
 
-#ifndef __SYNTHESIS__
-model_default_t b8[1];
-#else
-model_default_t b8[1] = {0.02334324};
-#endif
+weight_default_t b4[1] = {0.02334324};
 
 #endif
