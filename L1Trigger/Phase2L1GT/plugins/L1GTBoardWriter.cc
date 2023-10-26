19bb3c1f34c277892d140c06e51a4c24aa8e22f4
Benjamin Huber <benjamin.huber@cern.ch>
2023-10-26 17:31:20 +0200

    Fake tmux2 algo bit patterns

--- L1Trigger/Phase2L1GT/plugins/L1GTBoardWriter.cc
+++ L1Trigger/Phase2L1GT/plugins/L1GTBoardWriter.cc
@@ -41,6 +41,9 @@ private:
   const std::vector<unsigned long long> algoBitMask_;
   const edm::EDGetTokenT<P2GTAlgoBlockCollection> algoBlocksToken_;
   l1t::demo::BoardDataWriter boardDataWriter_;
+
+  std::map<l1t::demo::LinkId, std::vector<ap_uint<64>>> linkData_;
+  std::size_t tmuxCounter_;
 };
 
 L1GTBoardWriter::L1GTBoardWriter(const edm::ParameterSet& config)
@@ -52,41 +55,54 @@ L1GTBoardWriter::L1GTBoardWriter(const edm::ParameterSet& config)
           config.getParameter<std::string>("outputFilename"),
           config.getParameter<std::string>("outputFileExtension"),
           9,
-          1,
+          2,
           config.getParameter<unsigned int>("maxLines"),
           [](const std::vector<unsigned int>& channels) {
             l1t::demo::BoardDataWriter::ChannelMap_t channelMap;
             for (unsigned int channel : channels) {
-              channelMap.insert({l1t::demo::LinkId{"Algos", channel}, {l1t::demo::ChannelSpec{1, 0, 0}, {channel}}});
+              channelMap.insert({l1t::demo::LinkId{"Algos", channel}, {l1t::demo::ChannelSpec{2, 0, 0}, {channel}}});
             }
             return channelMap;
-          }(channels_)) {}
+          }(channels_)),
+      linkData_(),
+      tmuxCounter_(0) {}
 
 void L1GTBoardWriter::analyze(const edm::Event& event, const edm::EventSetup& iSetup) {
-  l1t::demo::EventData eventData;
   const P2GTAlgoBlockCollection& algoBlocks = event.get(algoBlocksToken_);
 
   auto algoBlockIt = algoBlocks.begin();
   auto algoMaskIt = algoBitMask_.begin();
 
   for (unsigned int channel : channels_) {
-    std::vector<ap_uint<64>> bits(9, 0);
+    if (tmuxCounter_ == 0) {
+      linkData_[{"Algos", channel}] = std::vector<ap_uint<64>>(18, 0);
+    }
+
     for (std::size_t word = 0; word < 9; word++) {
       ap_uint<64> mask = algoMaskIt != algoBitMask_.end() ? *algoMaskIt++ : ~static_cast<unsigned long long>(0);
 
       for (std::size_t idx = 0; idx < 64 && algoBlockIt != algoBlocks.end(); idx++) {
-        bits[word].set(idx, algoBlockIt->decisionBeforeBxMaskAndPrescale() && mask.bit(idx));
+        linkData_[{"Algos", channel}][word + tmuxCounter_ * 9].set(
+            idx, algoBlockIt->decisionBeforeBxMaskAndPrescale() && mask.bit(idx));
         algoBlockIt++;
       }
     }
-
-    eventData.add({"Algos", channel}, bits);
   }
 
-  boardDataWriter_.addEvent(eventData);
+  if (tmuxCounter_ == 1) {
+    boardDataWriter_.addEvent(l1t::demo::EventData(linkData_));
+  }
+
+  tmuxCounter_ = (tmuxCounter_ + 1) % 2;
 }
 
-void L1GTBoardWriter::endJob() { boardDataWriter_.flush(); }
+void L1GTBoardWriter::endJob() {
+  if (tmuxCounter_ == 1) {
+    boardDataWriter_.addEvent(l1t::demo::EventData(linkData_));
+  }
+
+  boardDataWriter_.flush();
+}
 
 void L1GTBoardWriter::fillDescriptions(edm::ConfigurationDescriptions& descriptions) {
   edm::ParameterSetDescription desc;
